8f29d8d80c1f509171dc209419c9b4a451ce39de
Sijan666 <666majharulislam@gmail.com>
2026-03-08 21:46:19 +0600

    fixed : container size

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@
 
 const Container = ({children , className}) => {
     return (
-        <div className={`lg:w-330 w-[300px] mx-auto ${className}`}>{children}</div>
+        <div className={`lg:w-352.5 w-[300px] mx-auto ${className}`}>{children}</div>
     )
 }
 
