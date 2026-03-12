6d890cf5e50d5c8a441b6d03bd62b3850f86b4f2
fima0519 <sabrinaahamed.official@gmail.com>
2026-03-12 13:01:16 +0600

    added coming soon page

--- src/components/pages/ComingSoon.jsx
+++ src/components/pages/ComingSoon.jsx
@@ -6,39 +6,44 @@ const ComingSoon = () => {
   return (
     <section
       id="coming_soon"
-      className="min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
+      className="w-full"
       style={{ backgroundImage: `url(${comingSoonImage})` }}
     >
-      <div className="bg-white max-w-[771px] w-full">
+      <div className="min-h-screen max-w-[771px] mx-auto flex justify-center bg-cover bg-center bg-no-repeat relative">
+        <div className=" h-[407px] w-full mt-[212px] absolute">
+          {/* Heading */}
+          <h1 className="w-[771px] h-[145px] text-[100px] flex items-center justify-center font-black uppercase text-[#222222] tracking-tighter font-Jost">
+            Coming Soon
+          </h1>
 
-        {/* Heading */}
-        <h1 className="font-Jost w-[771px] text-[100px] font-black uppercase text-[#222222] leading-none tracking-tighter font-Jost mb-[34px]">
-          Coming Soon
-        </h1>
+          {/* Subtitle */}
+          <p className="font-Jost text-[14px] font-normal text-[#222222] text-center leading-[24px]  mb-[45px]">
+            Sorry, we couldn't find the page you where looking for. We suggest
+            that you
+            <br />
+            return to home page.
+          </p>
 
-        {/* Subtitle */}
-        <p className="font-Jost text-[14px] font-normal text-[#222222] text-center leading-[24px]  mb-[30px]">
-          Sorry, we couldn't find the page you where looking for. We suggest that you<br />
-          return to home page.
-        </p>
+          {/* Countdown */}
+          <div className="flex justify-center font-Jost mb-[49px]">
+            <CountdownTimer />
+          </div>
 
-        {/* Countdown */}
-        <div className="flex justify-center font-Jost mb-[54px]">
-          <CountdownTimer />
+          {/* Email + Join */}
+          <div className="flex justify-center gap-x-[20px] w-[720px]">
+            <input
+              type="email"
+              placeholder="Your email address"
+              className="flex-1 pt-[20px] pb-[16px] pl-[21px] box-border border border-[#E4E4E4] border-r-0 text-[14px] text-[#767676] bg-white outline-none font-Jost font-normal font-[14px] w-[550px] h-[60px]"
+            />
+            <button
+              className="bg-black text-white pt-[20px] pb-[16px] pl-[61px] pr-[69px] text-[14px] font-medium  uppercase font-Jost hover:bg-[#767676] transition-colors 
+          w-[150px] h-[60px]"
+            >
+              Join
+            </button>
+          </div>
         </div>
-
-        {/* Email + Join */}
-        <div className="flex">
-          <input
-            type="email"
-            placeholder="Your email address"
-            className="flex-1 px-4 py-3 border border-[#ddd] border-r-0 text-sm text-[#aaa] bg-[#fafafa] outline-none font-Jost font-normal font-[14px] w-[720px] h-[60px]"
-          />
-          <button className="bg-black text-white px-7 py-3 text-[14px] font-medium tracking-widest uppercase font-Jost hover:bg-[#333] transition-colors w-[150px] h-[60px]">
-            Join
-          </button>
-        </div>
-
       </div>
     </section>
   );
